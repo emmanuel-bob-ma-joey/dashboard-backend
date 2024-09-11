3f38e90f5371a424b465bf3e8f85140f0784ec92
Emmanuel Ma <emmanuel.ma@gmail.com>
2024-09-10 23:51:44 -0400

    delete req for /users

--- pages/api/users.js
+++ pages/api/users.js
@@ -4,12 +4,13 @@ export default async function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*"); // Adjust as needed
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  const dbConnect = dbo.getDb("finance_dashboard");
+
   switch (req.method) {
     case "OPTIONS":
       res.status(200).end();
       break;
     case "POST":
-      const dbConnect = dbo.getDb("finance_dashboard");
       const { uid, email } = req.body;
 
       try {
@@ -22,6 +23,31 @@ export default async function (req, res) {
         res
           .status(500)
           .json({ message: "Error creating user", error: error.message });
+      }
+      break;
+    case "DELETE":
+      const { uid: userIdToDelete } = req.query;
+
+      try {
+        // Delete user from users collection
+        await dbConnect.collection("users").deleteOne({ uid: userIdToDelete });
+
+        // Delete user's portfolio
+        await dbConnect
+          .collection("portfolio")
+          .deleteMany({ user: userIdToDelete });
+
+        // Delete user's watchlist
+        await dbConnect
+          .collection("watchlist")
+          .deleteMany({ user: userIdToDelete });
+
+        res
+          .status(200)
+          .json({ message: "User and associated data deleted successfully" });
+      } catch (error) {
+        console.error("Error deleting user data:", error);
+        res.status(500).json({ error: "Failed to delete user data" });
       }
       break;
     default:
